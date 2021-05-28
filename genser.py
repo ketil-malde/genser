b3b536cb50413bc84ad20e250a3a009f455fe468
Ketil Malde <ketil@malde.org>
2021-05-28 16:15:29 +0200

    Add fourth distr, results seem...okay?

--- genser.py
+++ genser.py
@@ -21,49 +21,64 @@ def estimate(hist):
 from scipy.stats import norm
 
 # assign a histogram to three distrs and re-estimate parameters
+POISSON = False
 def expmax(distr, hist):
     h0 = {}
     h1 = {}
     h2 = {}
     h3 = {}
-    mu, sd, k0, k1, k2, k3 = distr
-    sd0, sd1, sd2, sd3 = sqrt(mu/2), sqrt(mu), sqrt(mu*2), sqrt(mu*3)
+    h4 = {}
+    mu, sd0, sd1, sd2, sd3, sd4, k0, k1, k2, k3, k4 = distr
     # assign histogram to distrib
+    pz = (k0+k1+k2+k3+k4)/1000
     for val,cnt in hist.items():
-        pz = 0.0001  # uniform
-        p0 = k0 * norm.pdf((val+0.5-mu/2)/sd0)  # prob of val under N(mu/2, sd/2)
-        p1 = k1 * norm.pdf((val+0.5-mu  )/sd1)
-        p2 = k2 * norm.pdf((val+0.5-mu*2)/sd2)
-        p3 = k3 * norm.pdf((val+0.5-mu*3)/sd3)
-        ptot = p0 + p1 + p2 + p3 + pz
+        p0 = k0/sd0 * norm.pdf((val+0.5-mu/2)/sd0)  # prob of val under N(mu/2, sd/2)
+        p1 = k1/sd1 * norm.pdf((val+0.5-mu  )/sd1)
+        p2 = k2/sd2 * norm.pdf((val+0.5-mu*2)/sd2)
+        p3 = k3/sd3 * norm.pdf((val+0.5-mu*3)/sd3)
+        p4 = k4/sd4 * norm.pdf((val+0.5-mu*4)/sd4)
+        ptot = p0 + p1 + p2 + p3 + p4 + pz
         h0[val] = cnt*p0/ptot
         h1[val] = cnt*p1/ptot
         h2[val] = cnt*p2/ptot
         h3[val] = cnt*p3/ptot
-    # estimate the parameters
+        h4[val] = cnt*p4/ptot
+    # re-estimate the parameters
     mu0, sd0 = estimate(h0)
     mu1, sd1 = estimate(h1)
     mu2, sd2 = estimate(h2)
     mu3, sd3 = estimate(h3)
-    print('  est:', (mu0,sd0), (mu1,sd1), (mu2,sd2), (mu3,sd3))
+    mu4, sd4 = estimate(h4)
     # weight estimate
-    n0, n1, n2, n3 = sum(h0.values()), sum(h1.values()), sum(h2.values()), sum(h3.values())
-    return ((mu0*2*n0 + mu1*n1 + mu2/2*n2 + mu3/3*n3)/(n0+n1+n2+n3) , (sd0*2*n0 + sd1*n1 + sd2*n2/2 + sd3*n3/3)/(n0+n1+n2+n3), n0, n1, n2, n3)
+    n0, n1, n2, n3, n4 = sum(h0.values()), sum(h1.values()), sum(h2.values()), sum(h3.values()), sum(h4.values())
+    new_mu = mu1 # (mu0*2*n0 + mu1*n1 + mu2/2*n2 + mu3/3*n3)/(n0+n1+n2+n3)
+    print(f'estimated mus    {mu0:.1f} {mu1:.1f} {mu2:.1f} {mu3:.1f} {mu4:.1f}; estimated sigmas {sd0:.1f} {sd1:.1f} {sd2:.1f} {sd3:.1f} {sd4:.1f}\r', end='')
+    if POISSON:
+        sd0 = sqrt(mu0)
+        sd1 = sqrt(mu1)
+        sd2 = sqrt(mu2)
+        sd3 = sqrt(mu3)
+        sd4 = sqrt(mu4)
+        print(f'poisson sigmas {sd0:.1f} {sd1:.1f} {sd2:.1f} {sd3:.1f} {sd4:.1f}')
+    return (new_mu , sd0, sd1, sd2, sd3, sd4, n0, n1, n2, n3, n4)
 
 # criterion for end of convergence
 def same(d0, d1):
-    mu0, sd0, _, _, _, _ = d0
-    mu1, sd1, _, _, _, _ = d1
-    return(abs(mu0-mu1) < 0.05 and abs(sd1-sd0) < 0.05)
+    mu0, sd0, _, _, _, _, _, _, _, _, _ = d0
+    mu1, sd1, _, _, _, _, _, _, _, _, _ = d1
+    return(abs(mu0-mu1) < 0.1 and abs(sd1-sd0) < 0.1)
 
 def errors(dist, hist):
     errs = {}
-    mu, sd, k0, k1, k2, k3 = dist
+    mu, sd0, sd1, sd2, sd3, sd4, k0, k1, k2, k3, k4 = dist
+    pz = (k0+k1+k2+k3+k4)/1000
     for val, cnt in hist.items():
-        e = cnt - (  k0 * norm.pdf( (val+0.5-mu/2)/(sd/2))
-                   + k1 * norm.pdf( (val+0.5-mu)  / sd   )
-                   + k2 * norm.pdf( (val+0.5-mu*2)/(sd*2))
-                   + k3 * norm.pdf( (val+0.5-mu*3)/(sd*3)))
+        e = cnt - (  k0/sd0 * norm.pdf( (val+0.5-mu/2)/sd0)
+                   + k1/sd1 * norm.pdf( (val+0.5-mu)  /sd1)
+                   + k2/sd2 * norm.pdf( (val+0.5-mu*2)/sd2)
+                   + k3/sd3 * norm.pdf( (val+0.5-mu*3)/sd3)
+                   + k4/sd4 * norm.pdf( (val+0.5-mu*4)/sd4)
+                   + pz)
         errs[val] = e
     return errs
 
--- test2.py
+++ test2.py
@@ -13,29 +13,39 @@ for l in sys.stdin.readlines():
 # estimate and compare        
 
 mu_, sd_ = G.estimate(hist) # wtf? tmp?
-dist = mu_, sd_, 1000.0, 1000.0, 1000.0, 1000.0
-print(dist)
+dist = mu_, sd_, sd_, sd_, sd_, sd_, 1000.0, 2000.0, 1000.0, 1000.0, 1000.0
+# print(dist)
 
 dist2 = G.expmax(dist, hist)
-print('**',dist2)
+# print('**',dist2)
 
 while(not G.same(dist,dist2)):
       dist = dist2
       dist2 = G.expmax(dist, hist)
-      print('\r',dist2,end='')
-      es = G.errors(dist2, hist)
-      print('  root sum square errs:', sqrt(sum([e*e for e in es.values()])))
+#      es = G.errors(dist2, hist)
+#      print('\r',dist2,end='')
+#      print('  root sum square errs:', sqrt(sum([e*e for e in es.values()])))
 #     print('  dist errs:', G.estimate(es))
 
-print('\nFinal:')      
-print(dist2)      
+print('\n\nFinal:\n')
+mu, sd0, sd1, sd2, sd3, sd4, k0, k1, k2, k3, k4 = dist2
 
-mu, sd, k0, k1, k2, k3 = dist
+print(f'mus    {mu/2:3.1f} {mu:3.1f} {mu*2:3.1f} {mu*3:3.1f} {mu*4:.1f}')
+print(f'sigmas {sd0:3.1f} {sd1:3.1f} {sd2:3.1f} {sd3:3.1f} {sd4:3.1f}')
+print(f'counts {int(k0):10} {int(k1):10} {int(k2):10} {int(k3):10} {int(k4):10}')
+print('')
 
 from scipy.stats import norm
 
-for k in range(1,100):
-    e1 = k0*norm.pdf((k+0.5-mu/2)/(sd/2))
-    e2 = k1*norm.pdf((k+0.5-mu)/sd)
-    e3 = k2*norm.pdf((k+0.5-mu*2)/(sd*2))
-    print(k, hist[k], 'pred:', e1, e2, e3, 'err:', hist[k]-(e1+e2+e3))
+for k in range(1,150):
+    e0 = k0/sd0 * norm.pdf((k+0.5-mu/2)/sd0) # fuck: normalize by x width (area!)
+    e1 = k1/sd1 * norm.pdf((k+0.5-mu)  /sd1)
+    e2 = k2/sd2 * norm.pdf((k+0.5-mu*2)/sd2)
+    e3 = k3/sd3 * norm.pdf((k+0.5-mu*3)/sd3)    
+    e4 = k4/sd4 * norm.pdf((k+0.5-mu*3)/sd3)
+    err = int(hist[k]-(e0+e1+e2+e3+e4))
+    if err < 0:
+        bar = '*'*(int(10*(-err)/hist[k]))
+    else:
+        bar = '-'*(int(10*err/hist[k]))
+    print(f'{k:03} {int(hist[k]):10} pred: {int(e0):10} {int(e1):10} {int(e2):10} {int(e3):10} {int(e4):10} err: {err:10}  {bar}')
